05cfec69ea630681733a8360a059ce4987b08f72
Ana-Stefania Gheorghe <ana-stefaniagheorghe@Ana-Stefanias-MacBook-Pro.local>
2022-02-05 22:18:34 +0100

    remove comments

--- src/firebase-api/index.js
+++ src/firebase-api/index.js
@@ -1,13 +1,9 @@
 import { collection, getDocs, orderBy, query, doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore"; 
 
 
-// get all media
 export const getMediaFirebase = async (db) => {
     const q = query(collection(db, "media"), orderBy("timestamp", "desc"));
-
-    
     const querySnapshot = await getDocs(q)
-
     const media = [];
     querySnapshot.forEach((doc) => {
       media.push({...doc.data(), mediaId: doc.id });
@@ -16,13 +12,8 @@ export const getMediaFirebase = async (db) => {
   }
 
 export const addTagFirebase = async(db, mediaId, tag) => {
-    console.log('tag is', tag);
-    console.log('mediaId is', mediaId);
     const mediaRef = doc(db, "media", mediaId);
-
     await updateDoc(mediaRef, {
-        // tags: arrayUnion(tag)
-
         tags: arrayUnion(tag)
     });
 }
@@ -31,8 +22,6 @@ export const removeTagFirebase = async(db, mediaId, tag) => {
     const mediaRef = doc(db, "media", mediaId);
 
     await updateDoc(mediaRef, {
-        // tags: arrayUnion(tag)
-
         tags: arrayRemove(tag)
     });
 }
